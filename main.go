10eeab3a6db2842ae9f1df1fbeba0c23b34bf621
wangrui <1257983649@qq.com>
2020-05-19 10:16:55 +0800

    初始化
    Signed-off-by: wangrui <1257983649@qq.com>

---
+++ main.go
@@ -0,0 +1,20 @@
+package main
+
+import (
+	_ "cms/models"
+	_ "cms/routers"
+	"github.com/astaxie/beego"
+	_ "github.com/astaxie/beego/session/mysql"
+	_ "github.com/go-sql-driver/mysql"
+)
+
+type User struct {
+	Uid int
+	Pwd string
+}
+
+func main() {
+	//orm.RegisterDataBase("default","mysql","root:123456@tcp(localhost:3306)/cmssystem?charset=utf8")
+	beego.BConfig.WebConfig.Session.SessionOn = true //开启session
+	beego.Run()
+}
